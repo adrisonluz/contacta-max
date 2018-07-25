7f397181faf2f9a679c6d6c9b6b0a181752bfef7
Adrison Luz <adrison.luz@vortigo.com.br>
2018-07-25 12:31:28 -0300

    Criado migration e model de produtos
    Criado model Products. Criado migration para a criação automática da tabela de produtos na instalação do projeto.

---
+++ database/migrations/2018_07_25_151353_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sku')->unique();
+            $table->string('name');            
+            $table->longText('description')->nullable();
+            $table->integer('quantity')->default(0);
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
